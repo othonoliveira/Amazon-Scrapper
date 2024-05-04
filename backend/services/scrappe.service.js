fe6d876b0c6b427fedce864a861ce7d4b205a478
Othon Oliveira <othonoliveira08@gmail.com>
2024-05-04 09:22:08 -0300

    Improving readability and adding other returning status

--- backend/services/scrappe.service.js
+++ backend/services/scrappe.service.js
@@ -1,23 +1,48 @@
 const scrappe = require('../models/scrappe')
-const { JSDOM } = require('jsdom')
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom
+const virtualConsole = new jsdom.VirtualConsole();
+virtualConsole.sendTo(console, { omitJSDOMErrors: true });
+virtualConsole.on("jsdomError", (err) => {
+  if (err.message !== "Could not parse CSS stylesheet") {
+    console.error(err);
+  }
+});
 
 const getAmazonProducts = async (keyword) => {
   const data = await scrappe(keyword);
 
-  const html = new JSDOM(data)
-  const htmlProducts = html.window.document.getElementsByClassName("a-section a-spacing-base");
-  var arr = [].slice.call(htmlProducts)
+  const { window: { document } } = new JSDOM(data, { virtualConsole })
+
+  const productsCount = document.getElementsByClassName("a-section a-spacing-base").length;
+
+  if (productsCount <= 0) return { status: 404, message: 'No products found' }
 
   const products = [];
 
-  arr.forEach((element, index) => {
-    const image = html.window.document.getElementsByClassName("s-image")[index].getAttribute('src');
-    const title = html.window.document.getElementsByClassName("a-size-base-plus a-color-base a-text-normal")[index].innerHTML;
-    const rating = html.window.document.getElementsByClassName("a-icon-alt")[index].innerHTML;
-    const reviwes = html.window.document.getElementsByClassName("a-size-base s-underline-text")[index].innerHTML;
+  for (var index = 0; index < productsCount; index++) {
+    const image = document.getElementsByClassName("s-image")[index]
+      .getAttribute('src') ?? 'Not Found';
 
-    products.push({ image, title, rating, reviwes })
-  });
+    var title = 'Not Found'
+    if (document.getElementsByClassName("a-size-base-plus a-color-base a-text-normal")[index]) {
+      var title = document
+        .getElementsByClassName("a-size-base-plus a-color-base a-text-normal")[index].innerHTML;
+    }
+
+    var rating = 'Not Found';
+    if (document.getElementsByClassName("a-icon-alt")[index]) {
+      var rating = document.getElementsByClassName("a-icon-alt")[index].innerHTML;
+    }
+
+    var reviews = 'Not Found';
+    if (document.getElementsByClassName("a-size-base s-underline-text")[index]) {
+      var reviews = document.getElementsByClassName("a-size-base s-underline-text")[index]
+        .innerHTML;
+    }
+    
+    products.push({ image, title, rating, reviews });
+  }
 
   return { status: 200, message: products };
 };
